e58f9fedcd23d9d5ce12d4cfb9692941d3e8fc64
Birhane Kahsay <birhane.kahsay99@gmail.com>
2023-10-07 17:32:31 +0200

    modification on certificates2

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -9,12 +9,16 @@ const Certificate = () => {
           <p className='text-xl'>Here are my certificates I have earned </p>
         </span>
         <div className='grid grid-cols-1 gap-6 md:grid-cols-4 md:gap-4'>
-           <article key={certificates[0].id} className='relative w-full border rounded-md border-green-950'>
-                  <a href={certificates[0].certificateLink} className='flex flex-col items-center justify-center w-full hover:cursor-pointer'>
-                    <img src='pdf/Certificate1.jpg' alt={certificates[0].certificateIssuer}/> 
+          {
+            certificates.map((certificate)=>{
+              return <article key={certificate.id} className='relative w-full border rounded-md border-green-950'>
+                  <a href={certificate.certificateLink} className='flex flex-col items-center justify-center w-full hover:cursor-pointer'>
+                    <img src={certificate.certificateImage} alt={certificate.certificateIssuer}/> 
                     <i className='w-1/2 p-2 mt-2 mb-2 text-white rounded-full bg-slate-950 hover:transition hover:translate-x-4 hover:bg-blue-800'>View</i>
                  </a>
               </article>
+            })
+          }
         </div>
     </section>
   )
